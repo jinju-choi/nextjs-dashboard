c3d81c02aab557b1f686e083a3e2288405618e61
jinju-choi <cjj4643@gmail.com>
2025-02-21 17:49:24 +0900

    폼 작업중

---
+++ app/lib/actions.ts
@@ -0,0 +1,11 @@
+'use client';
+
+export async function createInvoice(formData: FormData) {
+	const rawFormData = {
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	}
+
+	console.log(rawFormData);
+}
